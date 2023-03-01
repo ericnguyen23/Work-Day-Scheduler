01b132739d968a842c6452f7fb84fec16ef08740
Eric <ericnguyen23@gmail.com>
2023-03-01 14:13:50 -0800

    Added interval to update current time

--- script.js
+++ script.js
@@ -63,6 +63,12 @@ $(document).ready(function () {
   }
 
   // TODO: Add code to display the current date in the header of the page.
-  var todaysDate = dayjs().format("dddd, MMMM D YYYY, h:mm a");
-  document.getElementById("currentDay").textContent = todaysDate;
+
+  // update time every second
+  function displayCurrTime() {
+    var todaysDate = dayjs().format("dddd, MMMM D YYYY, h:mm a");
+    document.getElementById("currentDay").textContent = todaysDate;
+  }
+  displayCurrTime();
+  setInterval(displayCurrTime, 1000);
 });
